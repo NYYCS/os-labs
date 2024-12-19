af97b664f782f8c8e56444e3aba8b7d7cc960bc0
root <root@hwsrv-1238620.hostwindsdns.com>
2024-12-19 06:22:36 +0000

    pteprint pass

--- kernel/vm.c
+++ kernel/vm.c
@@ -437,3 +437,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+pteprint(pagetable_t pagetable, int depth)
+{
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+
+      for(int k = 0; k < depth; k++)
+        printf(" ..");
+      printf(" ..%d: pte %p pa %p\n", i, pte, child);
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0)
+        pteprint((pagetable_t)child, depth+1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  pteprint(pagetable, 0);
+}
